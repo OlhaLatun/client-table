cdae7c76ae1eca04fce7fcb544ca187d46618329
Olga Latun <olha.latun@gmail.com>
2021-09-17 09:51:19 +0300

    added disabled attributes to next/prev buttons in  PageControls component

--- src/Components/PageControls/PageControls.js
+++ src/Components/PageControls/PageControls.js
@@ -5,8 +5,12 @@ export default function PageControls({ clientsNumber, getPage }) {
 
     const [page, setPage] = useState(0)
     const pages = clientsNumber < 20 ? 1 : Math.floor(clientsNumber / 20)
+    const disabled = clientsNumber < 20 ? true : false
 
-    useEffect(() => { getPage(page) }, [page])
+
+    useEffect(() => {
+        getPage(page)
+    }, [page])
 
     const onClick = ({ target }) => {
         setPage(+target.id)
@@ -14,17 +18,19 @@ export default function PageControls({ clientsNumber, getPage }) {
 
     const prevClick = () => {
         setPage(prevState => prevState > 0 ? prevState - 1 : 0)
+
     }
 
     const nextClick = () => {
         setPage(prevState => prevState < 5 ? prevState + 1 : 5)
+
     }
 
     return (
         <div className='page-controls'>
-            <button className='btn' id='previous' onClick={() => prevClick()}> previous </button>
+            <button className='btn' id='previous' disabled={disabled} onClick={(e) => prevClick(e)}> previous </button>
             {[...Array(pages)].map((page, i) => <button className='btn' key={i} id={i} onClick={(e) => onClick(e)}> {i + 1} </button>)}
-            <button className='btn' id='next' onClick={() => nextClick()}> next </button>
+            <button className='btn' id='next' disabled={disabled} onClick={(e) => nextClick(e)}> next </button>
         </div>
     )
 }
